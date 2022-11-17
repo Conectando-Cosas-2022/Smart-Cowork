9912983d6912638ce3f5930d271c7a1ff1188c0e
pilarbusquets12 <112672256+pilarbusquets12@users.noreply.github.com>
2022-11-16 22:06:27 -0300

    Prueba aislada conexión a WIFI

---
+++ ConectarWifi/ConectarWifi.ino
@@ -0,0 +1,42 @@
+/*
+ *  This sketch demonstrates how to scan WiFi networks.
+ *  The API is almost the same as with the WiFi Shield library,
+ *  the most obvious difference being the different file you need to include:
+ */
+#include "WiFi.h"
+
+
+// Replace with your network credentials
+const char* ssid = "HUAWEI-IoT";
+const char* password = "ORTWiFiIoT";
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    Serial.println("Setup done");
+}
+
+void loop()
+{
+   initWiFi();
+
+  Serial.println("se conecto :)");
+    
+}
